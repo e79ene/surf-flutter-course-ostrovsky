7b2954daac5832eda5a73153762f14509243e75f
Eugene S. Ostrovsky <eso@mail.ru>
2021-03-10 06:08:09 +0000

    Build counter for stateless & stateful widgets.

--- lib/main.dart
+++ lib/main.dart
@@ -12,54 +12,54 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: MyFirstWidget(),
+      home: MyFullWidget(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
-
-  final String? title;
-
+// ignore: must_be_immutable
+class MyFirstWidget extends StatelessWidget {
+  static int staticCounter = 0;
+  int inWidgetCounter = 0;
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    staticCounter++;
+    inWidgetCounter++;
+    print('Counter staic:$staticCounter in widget:$inWidgetCounter');
+
+    return Container(
+      child: Center(
+        child: Text('Hello Stateless!'),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+// ignore: must_be_immutable
+class MyFullWidget extends StatefulWidget {
+  static int staticCounter = 0;
+  int inWidgetCounter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  @override
+  _MyFullWidgetState createState() => _MyFullWidgetState();
+}
+
+class _MyFullWidgetState extends State<MyFullWidget> {
+  int inStateCounter = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title!),
+    MyFullWidget.staticCounter++;
+    widget.inWidgetCounter++;
+    inStateCounter++;
+    print('Counter staic:${MyFullWidget.staticCounter}'
+        ' in widget:${widget.inWidgetCounter} in state:$inStateCounter');
+
+    return Container(
+      child: Center(
+        child: Text('Hello Stateful!'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
