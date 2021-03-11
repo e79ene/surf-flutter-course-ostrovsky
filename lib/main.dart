76af269c1349b2df7c4647ac01b4bd836d18c0fa
Eugene S. Ostrovsky <eso@mail.ru>
2021-03-11 06:00:35 +0000

    4.2.2 Set SightListScreen as application home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/screen/sight_list_screen.dart';
 
 void main() {
   runApp(App());
@@ -9,71 +10,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Заголовок',
-      home: MyFirstWidget(),
-    );
-  }
-}
-
-// ignore: must_be_immutable
-class MyFirstWidget extends StatelessWidget {
-  static int staticCounter = 0;
-  int inWidgetCounter = 0;
-
-  /* ОШИБКА: undefined name 'context'
-  Type contextType() {
-    return context.runtimeType;
-  }
-  */
-
-  @override
-  Widget build(BuildContext context) {
-    staticCounter++;
-    inWidgetCounter++;
-    print('Counter staic:$staticCounter in widget:$inWidgetCounter');
-
-    return Container(
-      child: Center(
-        child: Text('Hello Stateless!'),
-      ),
-    );
-  }
-}
-
-// ignore: must_be_immutable
-class MyFullWidget extends StatefulWidget {
-  static int staticCounter = 0;
-  int inWidgetCounter = 0;
-
-  /* ОШИБКА: undefined name 'context'
-  Type contextType() {
-    return context.runtimeType;
-  }
-  */
-
-  @override
-  _MyFullWidgetState createState() => _MyFullWidgetState();
-}
-
-class _MyFullWidgetState extends State<MyFullWidget> {
-  int inStateCounter = 0;
-
-  Type contextType() {
-    // Ok
-    return context.runtimeType;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    MyFullWidget.staticCounter++;
-    widget.inWidgetCounter++;
-    inStateCounter++;
-    print('Counter staic:${MyFullWidget.staticCounter}'
-        ' in widget:${widget.inWidgetCounter} in state:$inStateCounter');
-
-    return Container(
-      child: Center(
-        child: Text('Hello Stateful!'),
-      ),
+      home: SightListScreen(),
     );
   }
 }
