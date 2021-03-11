616b4011aa7364af7b1493bd4b961083633ac42a
Eugene S. Ostrovsky <eso@mail.ru>
2021-03-11 06:15:22 +0000

    4.2.2 Fill scaffold with elements.

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -8,6 +8,34 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('appBar'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.access_alarm),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Text('body'),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.ac_unit_outlined),
+        onPressed: () {},
+      ),
+      drawer: Drawer(
+        child: Text('drawer'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+          label: 'One',
+          icon: Icon(Icons.access_alarms),
+        ),
+        BottomNavigationBarItem(
+          label: 'Two',
+          icon: Icon(Icons.access_time),
+        ),
+      ]),
+    );
   }
 }
