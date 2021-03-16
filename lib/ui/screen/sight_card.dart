17611eeb8be0972abb86952149bdcbf1b63a297b
Eugene S. Ostrovsky <eso@mail.ru>
2021-03-16 20:36:10 +0100

    4.8 AspectRatio

--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -10,60 +10,73 @@ class SightCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Stack(children: [
-          Container(
-            width: double.infinity,
-            height: 100,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(9)),
-              color: imageStubColor,
-            ),
-            padding: EdgeInsets.all(9),
-            child: Text(
-              _sight.url,
-              style: imageStubUrlStyle,
-            ),
-          ),
-          Positioned(
-            top: 16,
-            left: 18,
-            child: Text(
-              _sight.type,
-              style: sightTypeStyle,
-            ),
-          ),
-          Positioned(
-            top: 5,
-            right: 8,
-            child: IconButton(
-              icon: Icon(Icons.favorite_border),
-              onPressed: () {},
-              color: onImageElementColor,
-            ),
-          ),
-        ]),
-        SizedBox(height: 16),
-        UnconstrainedBox(
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              maxWidth: MediaQuery.of(context).size.width * .78,
-            ),
+    return Container(
+      padding: EdgeInsets.only(bottom: 16),
+      child: AspectRatio(
+        aspectRatio: 3 / 2,
+        child: ClipRect(
+          child: OverflowBox(
+            maxHeight: double.infinity,
+            alignment: Alignment.topCenter,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Text(
-                  _sight.name,
-                  style: sightNameStyle,
+                Stack(children: [
+                  Container(
+                    width: double.infinity,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(9)),
+                      color: imageStubColor,
+                    ),
+                    padding: EdgeInsets.all(9),
+                    child: Text(
+                      _sight.url,
+                      style: imageStubUrlStyle,
+                    ),
+                  ),
+                  Positioned(
+                    top: 16,
+                    left: 18,
+                    child: Text(
+                      _sight.type,
+                      style: sightTypeStyle,
+                    ),
+                  ),
+                  Positioned(
+                    top: 5,
+                    right: 8,
+                    child: IconButton(
+                      icon: Icon(Icons.favorite_border),
+                      onPressed: () {},
+                      color: onImageElementColor,
+                    ),
+                  ),
+                ]),
+                SizedBox(height: 16),
+                UnconstrainedBox(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      maxWidth: MediaQuery.of(context).size.width * .78,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          _sight.name,
+                          style: sightNameStyle,
+                        ),
+                        Text(_sight.details),
+                      ],
+                    ),
+                  ),
                 ),
-                Text(_sight.details),
               ],
             ),
           ),
         ),
-      ],
+      ),
     );
   }
 }
