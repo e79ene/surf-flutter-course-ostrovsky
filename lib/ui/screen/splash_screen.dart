487f13ac3f97c47cdebaaf9b1f1c253756f3145f
Eugene S. Ostrovsky <eso@mail.ru>
2021-04-17 15:53:35 +0100

    Revert "7.4 Experimenting with Futures & isolates."
    This reverts commit cf507cf4f017a4a749c1da50a34ece9d478f29c0.

--- lib/ui/screen/splash_screen.dart
+++ lib/ui/screen/splash_screen.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:lorem_cutesum/lorem_cutesum.dart';
 import 'package:places/ui/res/my_icons.dart';
 import 'package:places/ui/res/themes.dart';
 import 'package:places/ui/svg_icon.dart';
@@ -51,54 +48,15 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   void _navigateToNext() async {
+    // Simulate some initialization
+    final dataLoading = Future(() {});
+
     // UX splash screen delay
     final uiWait = Future.delayed(Duration(seconds: 2), () {});
 
-    // _doWork('synchronously');
-
-    // Future(() => _doWork('in Future'));
-
-    compute(_doWork, 'in isolate');
-
+    await dataLoading;
     await uiWait;
 
     print('Переход на следующий экран');
   }
-}
-
-void _doWork(String method) {
-  print('Working $method...');
-
-  final strings = _generateStrings(1000000);
-  final res = _revertStrigs(strings);
-
-  print('Finished with ${res.length} stirngs');
-}
-
-Iterable<String> _generateStrings(int count) => [
-      for (int i = 0; i < count; i++) Cutesum.loremCutesum(words: 1),
-    ];
-
-Iterable<String> _revertStrigs(Iterable<String> strings) => [
-      for (final s in strings)
-        String.fromCharCodes(bubbleSort(s.runes.toList())),
-    ];
-
-List<int> bubbleSort(List<int> list) {
-  int n = list.length;
-  int i, step;
-  for (step = 0; step < n; step++) {
-    for (i = 0; i < n - step - 1; i++) {
-      if (list[i] > list[i + 1]) {
-        swap(list, i);
-      }
-    }
-  }
-  return list;
-}
-
-void swap(List list, int i) {
-  int temp = list[i];
-  list[i] = list[i + 1];
-  list[i + 1] = temp;
 }
